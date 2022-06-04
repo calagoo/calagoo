16d5d430c72f542f2015c4266aaf73db50d376e7
calagoo <100049646+calagoo@users.noreply.github.com>
2022-06-04 13:04:33 -0700

    Delete password_cracker directory

--- password_cracker/clear_system.py
+++
@@ -1,8 +0,0 @@
-import os
-from sys import platform
-# Clear the system
-def clear_system():
-    if platform == "linux" or platform == "linux2":
-        os.system("clear")
-    elif platform == "win32":
-        os.system("cls")
