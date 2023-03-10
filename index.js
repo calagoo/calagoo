ce9b1146d05ceb199e3dab56241fabd2b44c8d51
calagoo <robbierodriguez98@gmail.com>
2023-03-10 12:04:46 -0800

    orbital dynamics are finished, need to pretty up

--- index.js
+++ index.js
@@ -16,20 +16,26 @@ let dragY = 0;
 let rect = canvas.getBoundingClientRect();
 let pause = false;
 let drag = false;
-let G = 1; //6.67 * 10**-11
+let G = 6.67428e-11 // gravitational constant
+let AU = 149.6e6 // km
+// forces
+let gravAngle = 0;
+let gravPull = 0;
 
 // sun variables
-let sunMass = 333;
+let sunMass = 1.989*10**30; // kg
 let sunRadius = 20;
 let sunX = centerX;
 let sunY = centerY;
 
 // planet variables
 let planet = false; // planet is real boolean
-let planetRadius = 10;
-let planetMass = 1;
+let planetRadius = 7;
+let planetMass = 5.97219*10**24; // earth mass, kg
 let planetX = 0;
 let planetY = 0;
+let planet_arrX = [];
+let planet_arrY = [];
 let planetVX = 0;
 let planetVY = 0;
 let planetAX = 0;
@@ -54,6 +60,7 @@ function drawGame() {
     }
     if (planet) {
       drawPlanet();
+      drawAccelLine();
     }
   }
 }
@@ -68,28 +75,32 @@ function drawSun() {
   ctx.fillStyle = "yellow";
   ctx.arc(sunX, sunY, sunRadius, 0, 2 * Math.PI, false);
   ctx.fill();
-  ctx.lineWidth = 5;
+  ctx.lineWidth = 0;
   ctx.strokeStyle = "#FFF59D";
   ctx.stroke();
 }
 
 function planetMovement() {
   dist = ((planetX - sunX) ** 2 + (planetY - sunY) ** 2) ** 0.5;
-  if (dist < (sunRadius + planetRadius) / 2) {
+  // if the distance of the planet to the sun is slightly overlapping, then delete the planet
+  if (dist < (sunRadius * 1.9 + planetRadius * 1.9) / 2) {
     planet = false;
   }
-  // dist = dist/100
+  dist = (dist / 300) * AU * 1000; // convert to meters
   gravAngle = Math.atan2(sunY - planetY, sunX - planetX); // when the sun is to the right of the planet is 0 degrees
   gravPull = (G * planetMass * sunMass) / dist ** 2;
 
-  planetAX += (gravPull / planetMass) * Math.cos(gravAngle);
-  planetAY += (gravPull / planetMass) * Math.sin(gravAngle);
+  planetAX = (gravPull / planetMass) * Math.cos(gravAngle);
+  planetAY = (gravPull / planetMass) * Math.sin(gravAngle);
 
-  planetVX += (planetAX * 1) / fps;
-  planetVY += (planetAY * 1) / fps;
-  console.log("accx: " + planetAX, "accy: " + planetAY);
-  planetX += (planetVX * 1) / fps;
-  planetY += (planetVY * 1) / fps;
+  planetVX += planetAX * (1 / fps)*24;
+  planetVY += planetAY * (1 / fps)*24;
+
+  planet_arrX[planet_arrX.length] = planetX
+  planet_arrY[planet_arrY.length] = planetY
+
+  planetX += planetVX * (1 / fps)*24;
+  planetY += planetVY * (1 / fps)*24;
 }
 
 function drawPlanet() {
@@ -97,16 +108,42 @@ function drawPlanet() {
   ctx.fillStyle = "red";
   ctx.arc(planetX, planetY, planetRadius, 0, 2 * Math.PI, false);
   ctx.fill();
-  ctx.lineWidth = 5;
+  ctx.lineWidth = 0;
   ctx.strokeStyle = "red";
   ctx.stroke();
+
+  drawPath()
+
   planet = true;
 }
 
-function drawAccelLine(){
-
+function drawPath(){
+  for (let i = 0; i<planet_arrX.length;++i){
+    ctx.beginPath();
+    ctx.moveTo(planet_arrX[i-1], planet_arrY[i-1]);
+    ctx.lineTo(planet_arrX[i], planet_arrY[i]);
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "red";
+    ctx.stroke();
+  }
 }
 
+function drawAccelLine() {
+  accel = (planetAX ** 2 + planetAY ** 2) ** 0.5;
+  modifier = .001
+  acc_mod = accel/modifier
+  if (acc_mod > 45) {
+    acc_mod = 45
+  }
+  aX = ((acc_mod * Math.cos(gravAngle)) + planetX);
+  aY = ((acc_mod * Math.sin(gravAngle)) + planetY);
+  ctx.beginPath();
+  ctx.moveTo(planetX, planetY);
+  ctx.lineTo(aX, aY);
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "white";
+  ctx.stroke();
+}
 
 function drawProjLine() {
   ctx.beginPath();
@@ -136,9 +173,10 @@ function mouseDown() {
   planetVY = 0;
   planetAX = 0;
   planetAY = 0;
+  planet_arrX = []
+  planet_arrY = []
 
   drawPlanet();
-  console.log("Mouse Down   -- Coordinate x: " + mDx, "Coordinate y: " + mDy);
   return mDx, mDy;
 }
 
@@ -160,11 +198,11 @@ function mouseUp() {
   let releaseDiff = ((mUx - mDx) ** 2 + (mUy - mDy) ** 2) ** 0.5;
   let releaseAngle = Math.atan2(mDy - mUy, mDx - mUx);
 
-  planetVX = -releaseDiff * Math.cos(releaseAngle);
-  planetVY = -releaseDiff * Math.sin(releaseAngle);
+  modifier = 50
 
-  console.log("Mouse Up     -- Coordinate x: " + mUx, "Coordinate y: " + mUy);
-  console.log("Difference: " + releaseDiff, "Angle: " + releaseAngle);
+  planetVX = -releaseDiff/modifier * Math.cos(releaseAngle);
+  planetVY = -releaseDiff/modifier * Math.sin(releaseAngle);
+
   pause = false;
   drag = false;
 }
