a98abe790536b4646e10f3ae9fe260e1c8ca2e12
calagoo <robbierodriguez98@gmail.com>
2023-03-13 14:29:41 -0700

    fixed pitch of animation and added PID for xPos

--- PID_game.js
+++ PID_game.js
@@ -25,15 +25,24 @@ function closure() {
     var ticks = 0;
     var flameColors = [];
     var height = 50;
-    var angle = 0;
     var heightLast = 0;
-    var pidOut = [0, 0];
+    var xPos = centerX;
+    var xPosLast = 0;
+    var angle = 0;
+    var pitch = 0;
     var density = 1.225; // kg/m^3
+
+    var pidOut_height = [0, 0];
+    var pidOut_x = [0, 0];
     var excessLift = 0;
     var accelY = 0;
     var velY = 0;
-    //// Plane Variables (roughly 737).
+    var accelX = 0;
+    var velX = 0;
+    var lift = 0;
+    var drag = 0;
 
+    //// Plane Variables (roughly from a Boeing 737).
     // for angles -10 to 10, every 2.5
     // using information from a 737 midspan airfoil at RE == 100k, http://airfoiltools.com/polar/details?polar=xf-b737b-il-100000
     const angle_array = [-10, -7.5, -5, -2.5, 0, 2.5, 5, 7.5, 10];
@@ -48,6 +57,7 @@ function closure() {
     const mass = 70535; // kg, mtow
     const vel = 200; // m/s
     const sref = 125; // m^2
+    const maxthrust = 64_400; // Newtons
 
     // GAME DRAWING MAIN LOOP
     function drawGame() {
@@ -55,21 +65,23 @@ function closure() {
         if (ticks > canvas.width) {
             ticks = 1;
         }
-        
+
         setTimeout(drawGame, dt);
         clearScreen();
         drawTargetLine();
-        planeMove_out = planeMovement(height, targetHeight, angle);
+        planeMove_out = planeMovement(height, targetHeight, angle,xPos);
         heightLast = height;
+        xPosLast = xPos;
         height = planeMove_out[0];
         angle = planeMove_out[1];
+        xPos = planeMove_out[2];
         drawTextData(height, targetHeight, angle);
 
         // short clamp to make sure the target height isn't too large or small (this is just a backup, the one in the mouseMove() function shouldnt fail)
-        if (targetHeight > 150){
+        if (targetHeight > 150) {
             targetHeight = 150
-        } 
-        else if (targetHeight < 15){
+        }
+        else if (targetHeight < 15) {
             targetHeight = 15
         }
 
@@ -83,9 +95,9 @@ function closure() {
     function heightConvert(ht) {
         return canvas.height - (canvas.height / 200) * ht;
     }
-    
-    function heightConvertInv(ht){
-        return 200 - (ht*200)/canvas.height;
+
+    function heightConvertInv(ht) {
+        return 200 - (ht * 200) / canvas.height;
     }
 
 
@@ -140,31 +152,46 @@ function closure() {
             i = i - ki * error * (dt / 1000);
             output = Math.max(low, Math.min(high, output));
         }
-        return [output, ierr];
+        return [output, ierr,p,i,d];
     }
 
-    function planeMovement(height, targetHeight, angle) {
+    function planeMovement(height, targetHeight, angle, xPos) {
 
-        // Classic PID
-        kp = 1;
-        ki = 0.001;
-        kd = .8;
-
-        pidOut = pid(
-            kp,
-            ki,
-            kd,
+        // // Classic PID
+        // kp = 1;
+        // ki = 0.001;
+        // kd = .8;
+        
+        
+        pidOut_x = pid(
+            kp = 20,
+            ki = 0.01,
+            kd = 50,
+            centerX,
+            xPos,
+            dt,
+            xPosLast,
+            pidOut_x[1],
+            (high = maxthrust/1000),
+            (low = 0),
+            (op0 = drag/1000) //bias
+        );
+            
+        pidOut_height = pid(
+            kp = 1,
+            ki = 0.001,
+            kd = 0.8,
             targetHeight,
             height,
             dt,
             heightLast,
-            pidOut[1],
+            pidOut_height[1],
             (high = 10),
             (low = -10),
             (op0 = 0.509249975) //bias, at this angle the lift == mass
         );
 
-        angle = pidOut[0];
+        angle = pidOut_height[0];
 
         var closestAngles = detectClose(angle, angle_array);
         var closestIndex = [
@@ -184,6 +211,16 @@ function closure() {
                     Math.min(Math.abs(closestAngles[0]), Math.abs(closestAngles[1]))) /
                 Math.max(Math.abs(closestAngles[0]), Math.abs(closestAngles[1]))
             );
+
+            var CD_interp = lerp(
+                CD[closestIndex[0]],
+                CD[closestIndex[1]],
+                1 -
+                (Math.abs(angle) -
+                    Math.min(Math.abs(closestAngles[0]), Math.abs(closestAngles[1]))) /
+                Math.max(Math.abs(closestAngles[0]), Math.abs(closestAngles[1]))
+            );
+
         } else {
             var CL_interp = lerp(
                 CL[closestIndex[0]],
@@ -192,19 +229,37 @@ function closure() {
                     Math.min(Math.abs(closestAngles[0]), Math.abs(closestAngles[1]))) /
                 Math.max(Math.abs(closestAngles[0]), Math.abs(closestAngles[1]))
             );
+
+            var CD_interp = lerp(
+                CD[closestIndex[0]],
+                CD[closestIndex[1]],
+                (Math.abs(angle) -
+                    Math.min(Math.abs(closestAngles[0]), Math.abs(closestAngles[1]))) /
+                Math.max(Math.abs(closestAngles[0]), Math.abs(closestAngles[1]))
+            );
         }
 
-        var lift = ((CL_interp * (density * vel ** 2)) / 2) * sref;
-
+        lift = ((CL_interp * (density * vel ** 2)) / 2) * sref;
+        drag = ((CD_interp * (density * vel ** 2)) / 2) * sref;
+        
+        thrust = pidOut_x[0]*1000
+        
         excessLift = lift - mass * 9.81;
-        accelY = excessLift / mass;
-        velY += (accelY * (dt/1000));
-        height += (velY * (dt/1000));
-        drawPlane(heightConvert(height));
-        return [height, angle];
+        accelY = (excessLift+thrust*Math.sin(angle*Math.PI/180)) / mass; // add lift and thrust together to get total upwards force
+        velY += (accelY * (dt / 1000));
+        height += (velY * (dt / 1000));
+        
+        accelX = (thrust - drag) / mass
+        velX += (accelX * (dt / 1000));
+        xPos += velX * (dt / 1000)
+
+        pitch = Math.atan2(velY,velX+vel)
+        console.log(pitch * 180 / Math.PI)
+        drawPlane(heightConvert(height),pitch);
+        return [height, angle, xPos];
     }
 
-    function drawPlane(height) {
+    function drawPlane(height,pitch) {
         var planeLength = 40;
         var planeHeight = 10;
 
@@ -212,57 +267,60 @@ function closure() {
 
         // Body
         ctx.save();
-        ctx.translate(centerX - planeLength / 2, height - planeHeight / 2);
-        ctx.rotate((-angle * Math.PI) / 180);
+        ctx.translate(xPos - planeLength / 2, height - planeHeight / 2);
+        ctx.rotate(-pitch);
         ctx.fillRect(0, 0, planeLength, planeHeight);
         ctx.restore();
     }
 
     function drawTextData(height, targetHeight, angle) {
-        ctx.font = "16px serif";
+        ctx.font = "16px monospace";
         ctx.fillStyle = "white";
-        ctx.fillText("Plane Data (Based off 737):", 1, 15);
-        ctx.fillText("Height: " + round(height, 1) + " m", 1, 30);
-        ctx.fillText(round(targetHeight,0) + " m", 1, targetHeight_pxl-2);
-        ctx.fillText("Angle of Attack: " + round(angle, 2) + " deg", canvas.width-175, 15);
-        ctx.fillText("Lift: " + round((excessLift+(mass*9.81)) / 1000, 2) + " kN", 1, 45);
-        ctx.fillText("Vertical Vel: " + round(velY, 2) + " m/s", 1, 60);
-        ctx.fillText("P: " + round(p, 1), 1, canvas.height-2);
-        ctx.fillText("I: " + round(i, 2), 70, canvas.height-2);
-        ctx.fillText("D: " + round(d, 3), 135, canvas.height-2);
+        ctx.fillText("Plane Data:", 1, 15);
+        ctx.fillText(`Height   = ${round(height, 1)} m`, 1, 30);
+        ctx.fillText(`Lift     = ${round((excessLift + (mass * 9.81)) / 1000, 2)} kN`, 1, 45);
+        ctx.fillText(`Velocity = ${round(velX+vel, 2)}, ${round(velY, 2)} m/s`, 1, 60);
+        
+        ctx.fillText(`AoA   = ${round(angle, 2)} deg`, canvas.width - 150, 30);
+        ctx.fillText(`Pitch = ${round(pitch*180/Math.PI, 2)} deg`, canvas.width - 150, 45);
+
+        ctx.fillText(round(targetHeight, 0) + " m", 1, targetHeight_pxl - 2);
+        // ctx.fillText("P: " + round(p, 1), 1, canvas.height - 2);
+        // ctx.fillText("I: " + round(i, 2), 70, canvas.height - 2);
+        // ctx.fillText("D: " + round(d, 3), 135, canvas.height - 2);
         ctx.font = "12px serif";
         ctx.save()
-        ctx.translate(canvas.width-3,targetHeight_pxl+16)
-        ctx.rotate(-90*Math.PI/180)
-        ctx.fillText("DRAG",0,0)
+        ctx.translate(canvas.width - 3, targetHeight_pxl + 16)
+        ctx.rotate(-90 * Math.PI / 180)
+        ctx.fillText("DRAG", 0, 0)
         ctx.restore()
     }
 
 
-    function moveTargetHeight(mousePosX,mousePosY){
+    function moveTargetHeight(mousePosX, mousePosY) {
         targetHeight = heightConvertInv(mousePosY)
-        }
+    }
 
 
-    PID_mouseDown = function mouseDown(){
+    PID_mouseDown = function mouseDown() {
         mouseUp_bool = false
         mouseDown_bool = true
         mousePosX_click = (window.event.pageX - canvas.offsetLeft);
         mousePosY_click = (targetHeight_pxl) - (window.event.pageY - canvas.offsetTop);
     }
-    
-    PID_mouseMove = function mouseMove(){
+
+    PID_mouseMove = function mouseMove() {
         mousePosX_drag = window.event.pageX - canvas.offsetLeft;
         mousePosY_drag = window.event.pageY - canvas.offsetTop;
-        if (mouseDown_bool){ // checks if the mouse is currently clicking/holding down
-            if (mousePosX_click > 475 && mousePosY_click < 20 && mousePosY_click > -20){ // checks that you clicked originally on the height drag ball
-                if (heightConvertInv(mousePosY_drag) <= 150 && heightConvertInv(mousePosY_drag) >= 15){ // doesn't track if you try to change height to too high or too low
-                    moveTargetHeight(mousePosX_drag,mousePosY_drag)
+        if (mouseDown_bool) { // checks if the mouse is currently clicking/holding down
+            if (mousePosX_click > 475 && mousePosY_click < 20 && mousePosY_click > -20) { // checks that you clicked originally on the height drag ball
+                if (heightConvertInv(mousePosY_drag) <= 150 && heightConvertInv(mousePosY_drag) >= 15) { // doesn't track if you try to change height to too high or too low
+                    moveTargetHeight(mousePosX_drag, mousePosY_drag)
                 }
             }
         }
     }
-    PID_mouseUp = function mouseUp(){
+    PID_mouseUp = function mouseUp() {
         mouseUp_bool = true
         mouseDown_bool = false
         console.log("Mouse Up")
